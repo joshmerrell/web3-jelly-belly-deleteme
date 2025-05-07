0e320bb3edb7d9fcb2c1d06c48091c41ea23e873
Joshua Merrell <84419054+joshmerrell@users.noreply.github.com>
2025-05-07 18:57:00 +0000

    Replace placeholder API with Jelly Belly API

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="astro/client" />
+
+export interface Flavor {
+    beanId: number;
+    groupName: string[];
+    ingredients: string[];
+    flavorName: string;
+    description: string;
+    colorGroup: string;
+    backgroundColor: string;
+    imageUrl: string;
+    glutenFree: boolean;
+    sugarFree: boolean;
+    seasonal: boolean;
+    kosher: boolean;
+}
